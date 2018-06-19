77158325586d386b6e98c00bb0ea9293f94a3d55
israrcs <israrcs5@gmail.com>
2018-06-19 14:29:51 +0500

    Implement todo app | --skip-ci

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Todo App';
+  newTodo: string;
+  todos: any;
+  todoObj: any;
+
+  constructor() {
+    this.newTodo = '';
+    this.todos = [];
+  }
+
+  addTodo(event) {
+    this.todoObj = {
+      newTodo: this.newTodo,
+      completed: false
+    };
+    this.todos.push(this.todoObj);
+    this.newTodo = '';
+    event.preventDefault();
+  }
+
+  deleteTodo(index) {
+    this.todos.splice(index, 1);
+  }
+
+  editTodo(index){
+    this.newTodo = this.todos[index].value;
+  }
+
+  deleteSelectedTodos() {
+    //need ES5 to reverse loop in order to splice by index
+    for(var i=(this.todos.length -1); i > -1; i--) {
+      if(this.todos[i].completed) {
+        this.todos.splice(i, 1);
+      }
+    }
+  }
 }
